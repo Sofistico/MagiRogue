af3dc997f6e076f0e3f0281940a376b5e1d4404c
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2020-12-09 18:26:37 -0300

    Minor change

--- MagiRogue/System/FOVHandler.cs
+++ MagiRogue/System/FOVHandler.cs
@@ -1,9 +1,8 @@
-﻿using GoRogue;
+﻿using System;
+using System.Linq;
+using GoRogue;
+using GoRogue.GameFramework;
 using GoRogue.MapViews;
-using MagiRogue.Entities;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace MagiRogue.System
 {
@@ -12,74 +11,25 @@ namespace MagiRogue.System
     /// </summary>
     public class FOVHandler
     {
-        private readonly Actor actorView;
-        private readonly Distance distanceMeasureView;
-        private FOV fieldOfView;
-        private Map map; // a field to hold information of the map that is being worked
-
         /// <summary>
-        /// The constructor for the FOVSystem
+        /// Possible states for the FOVVisibilityHandler to be in.
         /// </summary>
-        /// <param name="actor"></param>
-        /// <param name="distanceMeasure"></param>
-        public FOVHandler(Actor actor, Distance distanceMeasure)
+        public enum FovState
         {
-            actorView = actor;
-            distanceMeasureView = distanceMeasure;
-        }
-
-        /// <summary>
-        /// Initializes the Field Of View map, that this component should be based on
-        /// </summary>
-        /// <param name="fieldOfViewMap"></param>
-        public void Initialize([NotNull] IMapView<bool> fieldOfViewMap)
-        {
-            fieldOfView = new FOV(fieldOfViewMap);
-        }
-
-        /// <summary>
-        /// Resets the field of view component
-        /// </summary>
-        public void Reset() => fieldOfView = null;
-
-        /// <summary>
-        /// Calculates the FieldOfView component
-        /// </summary>
-        public void Calculate()
-        {
-            if (fieldOfView == null)
-                return;
-
-            fieldOfView.Calculate(actorView.Position, actorView.ViewRadius, distanceMeasureView);
-        }
-
-        /// <summary>
-        /// Returns true if the position is in the field of view of this actor
-        /// If the FieldOfView component is not yet initialized, it will return false
-        /// </summary>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public bool PositionInFieldOfView(Point position)
-        {
-            if (fieldOfView == null) return false;
-            return fieldOfView.BooleanFOV[position];
-        }
-
-        /// <summary>
-        /// Returns all positions that are in the field of view of this actor
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<Coord> PositionInFieldOfView()
-        {
-            if (fieldOfView == null) yield break;
-
-            for (int y = 0; y < fieldOfView.BooleanFOV.Width; y++)
-            {
-                for (int x = 0; x < fieldOfView.BooleanFOV.Height; x++)
-                {
-                    yield return new Coord(x, y);
-                }
-            }
+            /// <summary>
+            /// Enabled state -- FOVVisibilityHandler will actively set things as seen/unseen when appropriate.
+            /// </summary>
+            Enabled,
+            /// <summary>
+            /// Disabled state.  All items in the map will be set as seen, and the FOVVisibilityHandler
+            /// will not set visibility of any items as FOV changes or as items are added/removed.
+            /// </summary>
+            DisabledResetVisibility,
+            /// <summary>
+            /// Disabled state.  No changes to the current visibility of terrain/entities will be made, and the FOVVisibilityHandler
+            /// will not set visibility of any items as FOV changes or as items are added/removed.
+            /// </summary>
+            DisabledNoResetVisibility
         }
     }
 }
