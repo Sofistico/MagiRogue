0775ee18c4581fda5fbddf2c71a2d3f82580b256
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2020-12-18 16:09:02 -0300

    Created new project for testing

---
+++ MagiRogueTests/System/MapTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MagiRogue.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagiRogue.System.Tests
+{
+    [TestClass()]
+    public class MapTests
+    {
+        [TestMethod()]
+        public void MapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsTileWalkableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEntityAtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTileAtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTileAtTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateFOVTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateFOVTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
