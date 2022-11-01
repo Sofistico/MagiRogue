7db92170e105a52ed2711fee1331ea9dcf745ee2
Sofistico <joaomegamem@gmail.com>
2022-11-01 18:24:06 -0300

    Stuff

--- MagiRogue/Entities/Mind.cs
+++ MagiRogue/Entities/Mind.cs
@@ -1,10 +1,7 @@
 ﻿using MagiRogue.Data.Enumerators;
 using MagiRogue.Utils;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MagiRogue.Entities
 {
