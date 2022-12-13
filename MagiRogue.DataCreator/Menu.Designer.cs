17c8741ac0a0bbdd0637670ec5db5fb8277af533
Sofistico <joaomegamem@gmail.com>
2022-12-13 10:48:30 -0300

    Changed the form name to better match what it will be

--- MagiRogue.DataCreator/Menu.Designer.cs
+++ MagiRogue.DataCreator/Menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MagiRogue.DataCreator
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "Data Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ MagiRogue.DataCreator/Menu.cs
@@ -0,0 +1,15 @@
+namespace MagiRogue.DataCreator
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
