040f9d519b9e3722adb68abb90ace46d90c6938a
Sofistico <joaomegamem@gmail.com>
2023-05-03 15:40:53 -0300

    Fixing some other stuff!

--- MagiRogue/Utils/CombatUtils.cs
+++ MagiRogue/Utils/CombatUtils.cs
@@ -299,13 +299,12 @@ namespace MagiRogue.Utils
                         protection = 0;
                         break;
                 }
-                var damage =
-                    loopDamage
-                    - ((protection + Mrn.Exploding2D6Dice) * attack.PenetrationPercentage)
-                    + (defender.Body.Endurance * 0.5);
-                loopDamage = MathMagi.Round(damage);
+                var damageWithoutPenetration = loopDamage - (protection + Mrn.Exploding2D6Dice) + (defender.Body.Endurance * 0.5);
+                var penetrationDamage = damageWithoutPenetration * attack.PenetrationPercentage;
+                var finalDamage = damageWithoutPenetration + penetrationDamage;
+                finalDamage = MathMagi.Round(finalDamage);
 
-                totalDamage += loopDamage;
+                totalDamage += finalDamage;
             }
 
             return totalDamage;
