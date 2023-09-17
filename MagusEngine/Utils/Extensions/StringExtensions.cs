b8ad14339c83e69ad2338b27f69d4b6d6e50f88a
Sofistico <joaomegamem@gmail.com>
2023-09-16 22:48:18 -0300

    new extension for string

--- MagusEngine/Utils/Extensions/StringExtensions.cs
+++ MagusEngine/Utils/Extensions/StringExtensions.cs
@@ -52,5 +52,15 @@ namespace MagusEngine.Utils.Extensions
                 return null;
             }
         }
+
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
     }
 }
