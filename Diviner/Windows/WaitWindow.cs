7f7a36a4f6a164638b1728ea0e97f33c7d81877c
Sofistico <joaomegamem@gmail.com>
2024-05-22 15:30:15 -0300

    another small change

--- Diviner/Windows/WaitWindow.cs
+++ Diviner/Windows/WaitWindow.cs
@@ -15,6 +15,7 @@ namespace Diviner.Windows
         public WaitWindow() : base(waitTitle)
         {
             int yCount = 1;
+
             var wait1Hour = new MagiButton(string.Format(wait, CharExtension.GetCharHotkey(yCount), 1, "hour"), () =>
                 {
                     ActionManager.WaitForNTurns(Find.Universe.Time.GetNHoursFromTurn(1), (Actor)Find.CurrentMap?.ControlledEntitiy);
@@ -22,16 +23,15 @@ namespace Diviner.Windows
                 }, new SadRogue.Primitives.Point(ButtonWidth, yCount));
             var wait3Hour = new MagiButton(string.Format(wait, CharExtension.GetCharHotkey(yCount++), 3, "hours"), () =>
                 {
-                    ActionManager.WaitForNTurns(Find.Universe.Time.GetNHoursFromTurn(3),
-                        (Actor)Find.CurrentMap?.ControlledEntitiy);
+                    ActionManager.WaitForNTurns(Find.Universe.Time.GetNHoursFromTurn(3), (Actor)Find.CurrentMap?.ControlledEntitiy);
                     Hide();
                 }, new SadRogue.Primitives.Point(ButtonWidth, yCount));
             var wait6Hour = new MagiButton(string.Format(wait, CharExtension.GetCharHotkey(yCount++), 6, "hours"), () =>
                 {
-                    ActionManager.WaitForNTurns(Find.Universe.Time.GetNHoursFromTurn(6),
-                        (Actor)Find.CurrentMap?.ControlledEntitiy);
+                    ActionManager.WaitForNTurns(Find.Universe.Time.GetNHoursFromTurn(6), (Actor)Find.CurrentMap?.ControlledEntitiy);
                     Hide();
                 }, new SadRogue.Primitives.Point(ButtonWidth, yCount));
+
             _hotKeys.Add('a', (wait1Hour, true));
             _hotKeys.Add('b', (wait3Hour, true));
             _hotKeys.Add('c', (wait6Hour, true));
