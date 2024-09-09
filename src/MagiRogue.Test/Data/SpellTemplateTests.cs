0dff8c8e542f0e1bd345106f9bfc2e72991a8636
Sofistico <joaomegamem@gmail.com>
2024-09-09 15:36:22 -0300

    doing stuff

--- src/MagiRogue.Test/Data/SpellTemplateTests.cs
+++ src/MagiRogue.Test/Data/SpellTemplateTests.cs
@@ -8,21 +8,23 @@ namespace MagiRogue.Test.Data
 {
     public class SpellTemplateTests
     {
-        private readonly Spell missile = new("magic_missile",
-            "Magic Missile",
-            ArtMagic.Projection,
-            5,
-           "Mana Shaping",
-            magicCost: 1.0)
-        {
-            Proficiency = 1
-        };
+        private readonly Spell missile =
+            new(
+                "magic_missile",
+                "Magic Missile",
+                ArtMagic.Projection,
+                5,
+                "Mana Shaping",
+                magicCost: 1.0
+            )
+            {
+                Proficiency = 1
+            };
 
         [Fact]
         public void SpellTemplateSerializationTest()
         {
             var spell = DataManager.QuerySpellInData("magic_missile");
-
 
             string spellSerialized = JsonConvert.SerializeObject(spell, Formatting.Indented);
             Spell spellDeserialized = JsonConvert.DeserializeObject<Spell>(spellSerialized);
