bfc751a335ed9e968ed0a189da929a2882823eb1
Sofistico <joaomegamem@gmail.com>
2024-12-02 16:51:45 -0300

    Fix animation facytory

--- src/MagusEngine/Factory/AnimationFactory.cs
+++ src/MagusEngine/Factory/AnimationFactory.cs
@@ -5,12 +5,8 @@ using Newtonsoft.Json.Linq;
 
 namespace MagusEngine.Factory
 {
-    public static class AnimationFactory : test<string, JToken, AnimationBase>
+    public static class AnimationFactory
     {
-    }
 
-    public class test<TKey, TFunc, TReturn> where TKey : notnull
-    {
-        private readonly Dictionary<TKey, Func<TFunc, TReturn>> Turn = [];
     }
 }
