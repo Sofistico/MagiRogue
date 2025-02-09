dd0d15d2093662c47cdf3ed68fea7bba9ca911c4
Sofistico <joaomegamem@gmail.com>
2025-02-09 14:24:45 -0300

    some new work on the kaboom animation for better rendering of particles, hopefully but its still wip

--- src/MagusEngine/Core/Animations/KaboomAnimation.cs
+++ src/MagusEngine/Core/Animations/KaboomAnimation.cs
@@ -1,9 +1,14 @@
 using Arquimedes.Enumerators;
 using MagusEngine.Bus.UiBus;
+using MagusEngine.Components.EntityComponents;
+using MagusEngine.Components.TilesComponents;
 using MagusEngine.Core.Animations.Interfaces;
 using MagusEngine.Core.Entities;
+using MagusEngine.Core.MapStuff;
 using MagusEngine.Services;
+using MagusEngine.Systems;
 using MagusEngine.Utils;
+using SadConsole;
 using SadRogue.Primitives;
 
 namespace MagusEngine.Core.Animations
@@ -22,9 +27,17 @@ namespace MagusEngine.Core.Animations
         {
             var circle = GeometryUtils.CircleFromOriginPoint(originPos, Radius);
             var totalGlyphs = Glyphs.Length;
-            foreach (var item in Glyphs)
+            // foreach (var item in Glyphs)
+            // {
+            //     var particleGroup = new ParticlesGroup();
+            // }
+            foreach (var pos in circle.Points)
             {
-                var particleGroup = new ParticlesGroup();
+                var terrain = Find.CurrentMap.GetTileAt(pos);
+                var component = new ExtraAppearanceComponent(new ColoredGlyph(Color.GreenYellow, Color.LightBlue, Glyphs[0]));
+                var currentComp = terrain.GetComponent<ExtraAppearanceComponent>();
+                terrain.RemoveComponent(currentComp);
+                terrain.AddComponent(component);
             }
         }
     }
